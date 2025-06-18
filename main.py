ba5fcc17e5521c49b3854297a532be234219c633
leonaides <llim@isimarkets.com>
2025-06-18 16:25:46 +0800

    Refactoring the main.py

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-
-
 from sklearn.metrics import recall_score
-
-import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, roc_auc_score
-
 from sklearn.model_selection import GridSearchCV
-
-import seaborn as sns
 
 
 sample_dataset = r"C:\Users\dev_admin\Documents\health_dataset\dataset\heart.csv"
